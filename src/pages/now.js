4dbccd232c2d331813c378b77140e8d8fe932b02
Elliot Betancourt <elliot@elliotbetancourt.com>
2018-11-05 21:26:52 +0100

    add some placeholder pages

---
+++ src/pages/now.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const About = () => (
+    <Layout>
+        <div>
+            <h1>What Am I Up to...Now?</h1>
+            <p>
+                test test test
+            </p>
+        </div>
+    </Layout>
+);
+
+export default About;
---
+++ src/pages/uses.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const Uses = () => (
+    <Layout>
+        <div>
+            <h1>My Gear</h1>
+            <p>
+                test test test
+            </p>
+        </div>
+    </Layout>
+);
+
+export default Uses;
