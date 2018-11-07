877fc90cd99122e8d8b9d4c0de8c2c176262c45c
Elliot Betancourt <elliot@elliotbetancourt.com>
2018-11-07 22:37:11 +0100

    add sitemap and rss feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Elliot Betancourt',
+    siteUrl: 'https://elliotbetancourt.netlify.com',
     description: 'The home on the web of Elliot Betancourt, freelance web developer',
     tagLine: 'Act without doing; work without effort',
   },
@@ -37,6 +38,61 @@ module.exports = {
     },
     'gatsby-plugin-offline',
     'gatsby-transformer-remark',
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  url: site.siteMetadata.siteUrl + "/posts" + edge.node.frontmatter.slug,
+                  guid: site.siteMetadata.siteUrl + "/posts" + edge.node.frontmatter.slug,
+                  custom_elements: [{ "content:encoded": edge.node.html }],
+                })
+              })
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  filter: {frontmatter: { draft: { ne: true } }}
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      frontmatter {
+                        title
+                        date
+                        slug
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: "/rss.xml",
+            title: "Elliot Betancourt RSS Feed",
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-sitemap',
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-netlify',
   ],
--- src/posts/post1.md
+++ src/posts/post1.md
@@ -3,6 +3,7 @@ title: Our First Post
 slug: /first-post
 date: '2018-11-06'
 posttype: post
+draft: false
 ---
 
 ## Second Post
