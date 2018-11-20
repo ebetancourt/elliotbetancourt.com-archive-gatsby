81f4a6d74cbc6472fc4366992a911cdd16d434d8
Elliot Betancourt <elliot@elliotbetancourt.com>
2018-11-20 14:25:09 +0100

    super-simple responsive nav

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -14,9 +14,34 @@ const Header = ({ siteTitle }) => (
             textDecoration: 'none',
           }}
         >
-<h3>elliot betancourt</h3>
+          <h3>elliot betancourt</h3>
         </Link>
       </h1>
+      <div>
+        <input className={styles.menuToggle} id="menu-toggle" type="checkbox" />
+        <label className={styles.menuButtonContainer} htmlFor="menu-toggle">
+          <span>
+            <div className={styles.menuButton} />
+          </span>
+        </label>
+        <ul className={styles.menu}>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/now">Now</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/uses">Uses</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 )
